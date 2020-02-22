3e748fbfcfedd04d47d28a6f8cea87afd1e242bb
Jalena Lee <jalenalee@gmail.com>
2020-02-22 19:10:07 +0100

    assign2 branch

--- tutorials/tutorial4/todo-server/todo-server.js
+++ tutorials/tutorial4/todo-server/todo-server.js
@@ -1,4 +1,3 @@
-
 const http = require('http');
 const fs = require("fs");
 
@@ -6,50 +5,64 @@ const fs = require("fs");
 //Request represents the incoming request object
 //Response represents the outgoing response object
 //Remember, you can break this apart to make it look cleaner
-const server = http.createServer(function (request, response) {
-	console.log(request.url);
-	if(request.method === "GET"){
-		if(request.url === "/" || request.url === "/todo.html"){
-			//read the todo.html file and send it back
-			fs.readFile("todo.html", function(err, data){
-				if(err){
-					response.statusCode = 500;
-					response.write("Server error.");
-					response.end();
-					return;
-				}
-				response.statusCode = 200;
-				response.setHeader("Content-Type", "text/html");
-				response.write(data);
-				response.end();
-			});
-		}else if(request.url === "/todo.js"){
-			//read todo.js file and send it back
-			fs.readFile("todo.js", function(err, data){
-				if(err){
-					response.statusCode = 500;
-					response.write("Server error.");
-					response.end();
-					return;
-				}
-				response.statusCode = 200;
-				response.setHeader("Content-Type", "application/javascript");
-				response.write(data);
-				response.end();
-			});
-		//Add any more 'routes' and their handling code here
-		//e.g., GET requests for "/list", POST request to "/list"
-		}else{
-			response.statusCode = 404;
-			response.write("Unknwn resource.");
-			response.end();
-		}
-	}else if(request.method === "POST"){
-		//any handling in here
-		response.statusCode = 404;
-		response.write("Unknwn resource.");
-		response.end();
-	}
+const server = http.createServer(function(request, response) {
+    console.log(request.url);
+    if (request.method === "GET") {
+        if (request.url === "/" || request.url === "/todo.html") {
+            //read the todo.html file and send it back
+            fs.readFile("todo.html", function(err, data) {
+                if (err) {
+                    response.statusCode = 500;
+                    response.write("Server error.");
+                    response.end();
+                    return;
+                }
+                response.statusCode = 200;
+                response.setHeader("Content-Type", "text/html");
+                response.write(data);
+                response.end();
+            });
+        } else if (request.url === "/todo.js") {
+            //read todo.js file and send it back
+            fs.readFile("todo.js", function(err, data) {
+                if (err) {
+                    response.statusCode = 500;
+                    response.write("Server error.");
+                    response.end();
+                    return;
+                }
+                response.statusCode = 200;
+                response.setHeader("Content-Type", "application/javascript");
+                response.write(data);
+                response.end();
+            });
+            //Add any more 'routes' and their handling code here
+            //e.g., GET requests for "/list", POST request to "/list"
+        } else if (request.url === "/list") {
+            //read todo.js file and send it back
+            fs.readFile("todo.js", function(err, data) {
+                if (err) {
+                    response.statusCode = 500;
+                    response.write("Server error.");
+                    response.end();
+                    return;
+                }
+                response.statusCode = 200;
+                response.setHeader("Content-Type", "application/javascript");
+                response.write(data);
+                response.end();
+            });
+        } else {
+            response.statusCode = 404;
+            response.write("Unknwn resource.");
+            response.end();
+        }
+    } else if (request.method === "POST") {
+        //any handling in here
+        response.statusCode = 404;
+        response.write("Unknwn resource.");
+        response.end();
+    }
 });
 
 //Server listens on port 3000
